be23d861dbf7c0184871b52f89c6c41dd2ec4525
William Ross <will@spanner.org>
2016-09-01 12:28:25 +0100

    use ubuntu packages wherever sane

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Build and squash baseimage
 docker build -t rails4-base ./base
 ID=$(docker run -d rails4-base true)
-docker export $ID | docker import - rails4-base-squashed
+docker export $ID | docker import - rails4-base-reimport
 
 # Create onbuild image
 docker build -t rails4-onbuild ./onbuild
