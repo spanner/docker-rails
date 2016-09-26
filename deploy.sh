5889eeaf1c2a126527971d3fd42c70d3dcc4d307
William Ross <will@spanner.org>
2016-09-26 21:02:26 +0100

    expect docker-compose to define env

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@
 
 . VERSION
 
-docker login --email=$DOCKER_EMAIL --username=$DOCKER_USER -p $DOCKER_PASSWORD
 docker tag rails4-onbuild $DOCKER_TAG:latest
 docker push $DOCKER_TAG:latest
 
 if ! docker pull $DOCKER_TAG:$VERSION; then
   echo "Releasing new version: $VERSION"
-
   docker tag rails4-onbuild $DOCKER_TAG:$VERSION
   docker push $DOCKER_TAG:$VERSION
+
 else
   echo "Not overwriting existing version: $VERSION"
 fi
